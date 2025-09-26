a79e320061e502a759b3207cd9a057cecf6ecaf0
bf-yang <bfyang@link.cuhk.edu.hk>
2025-09-26 13:26:53 +0800

    stop tracking data_process and results

--- src/data_process/dataset_split.sh
+++
@@ -1,5 +0,0 @@
-# split cab dataset into train and test sets
-python src/data_process/split_dataset.py
-
-# split cab_lite dataset into train and test sets
-python src/data_process/split_dataset_cab_lite.py --dataset cab_lite
